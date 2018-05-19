54e773281c7ced6fe2a59a6d66236b201523b59c
Zhang <andrew-pph@outlook.com>
2018-05-20 01:22:43 +0800

    Initial commit

---
+++ settings.rb
@@ -0,0 +1,24 @@
+# blog settings
+
+$url_base = 'https://http://blindtiger.ml/blog/'
+
+$blog_title = 'BlindTiger\'s home'
+$blogger_name = 'Andrew Zhang'
+$blogger_intro = 'Have the courage to follow my heart and intuition. They somehow already know what I truly want to become. Everything else is secondary.'
+
+$copy_right = '2016--2018© Yeonji'
+
+# The zone show in articles
+$timezone = '+08:00'
+
+# These is used in RSS
+$rss_title = 'BlindTiger\'s home'
+$rss_description = 'Stay hungry,stay foolish.'
+
+# These Links will show in contact part.
+# to disable a link, just set it to nil
+$link_email = 'Andrew-pph@outlook.com'
+$link_facebook = nil
+$link_github = 'https://github.com/Blind-Tiger'
+$link_qq = nil
+$link_twitter = nil
